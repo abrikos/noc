7658c5b6d4a9448934d967ee45225136be5535e3
abrikos <abrikoz@gmail.com>
2020-04-11 13:39:05 +0900

    remove login menu

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -24,7 +24,7 @@ export default function Layout(props) {
         //{label: "ОУС", items: siteMap.filter(m => m.pages[0] === 'obedinyonnye-uchyonye-sovety' && m.pages.length>1).map(m => ({label: m.label, path: '/meeting/' + m.pages[1]}))},
         {label: 'ADMIN', path: '/admin/start', hidden: !(props.authenticatedUser && props.authenticatedUser.admin)},
         {label: `${(props.authenticatedUser && props.authenticatedUser.name)}`, path: '/cabinet', hidden: !props.authenticatedUser},
-        {label: 'Вход', path: '/login', hidden: props.authenticatedUser},
+        //{label: 'Вход', path: '/login', hidden: props.authenticatedUser},
         {label: 'Выход', onClick: props.logOut, hidden: !props.authenticatedUser},
     ];
 
