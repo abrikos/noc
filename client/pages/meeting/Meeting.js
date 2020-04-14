23f349eeb0e0124cab9f02b5dd4dc04f75164c03
abrikos <abrikoz@gmail.com>
2020-04-14 11:50:38 +0900

    fix meeting

--- client/pages/meeting/Meeting.js
+++ client/pages/meeting/Meeting.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from "react";
-import {Input} from "reactstrap";
+import {Input, Nav, NavItem} from "reactstrap";
 import meetingVoices from "client/meeting-voices";
 import PersonListSmall from "client/pages/persons/PersonListSmall";
 
 export default function Meeting(props) {
     const [data, setData] = useState();
+    const [voice, setVoice] = useState(-1);
     const [persons, setPersons] = useState([]);
+    console.log(document.cookie)
 
     useEffect(() => {
         props.api('/meeting/' + props.page)
@@ -15,6 +17,7 @@ export default function Meeting(props) {
     }, [props.page]);
 
     function selectDivision(voice) {
+        setVoice(voice)
         props.api('/person/list', {voice})
             .then(setPersons)
     }
@@ -22,10 +25,15 @@ export default function Meeting(props) {
     if (!data) return <div/>;
     return <div className="phone-book">
         <h1>{data.name}</h1>
+
         <div className="d-sm-block d-none">
-            {meetingVoices.map((d, i) => <span key={i} value={i} onClick={()=>selectDivision(i)}>{d}</span>)}
+            <Nav tabs>
+                <NavItem><span className={`nav-link ${voice===-1 ?'active':''}`} onClick={() => selectDivision('')}>Все</span></NavItem>
+                {meetingVoices.map((d, i) => <NavItem key={i}><span className={`nav-link  ${voice===i ?'active':''}`} onClick={() => selectDivision(i)}>{d}</span></NavItem>)}
+            </Nav>
+
         </div>
-        <Input type="select" onChange={e=>selectDivision(e.target.value)} className="d-sm-none d-block">
+        <Input type="select" onChange={e => selectDivision(e.target.value)} className="d-sm-none d-block">
             <option value={''}>Все</option>
             {meetingVoices.map((d, i) => <option key={i} value={i}>{d}</option>)}
         </Input>
