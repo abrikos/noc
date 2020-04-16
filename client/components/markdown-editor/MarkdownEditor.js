7a27da3e3f066821193e2ca68d025b6c6a04878b
abrikos <abrikoz@gmail.com>
2020-04-16 10:22:15 +0900

    fix markdown

--- client/components/markdown-editor/MarkdownEditor.js
+++ client/components/markdown-editor/MarkdownEditor.js
@@ -1,9 +1,9 @@
-import Editor from "@opuscapita/react-markdown";
 import React, {useEffect, useState} from "react";
 import "./markdown.sass"
 import MarkDown from "react-markdown";
 import PropTypes from "prop-types";
-import {FormFeedback, Input} from "reactstrap";
+import {Input} from "reactstrap";
+
 MarkdownEditor.propTypes = {
     value: PropTypes.string,
     name: PropTypes.string.isRequired,
@@ -14,25 +14,26 @@ MarkdownEditor.propTypes = {
 export default function MarkdownEditor(props) {
     const [value, setValue] = useState(props.value);
 
-    useEffect(()=>{
+    useEffect(() => {
         setValue(props.value)
-    },[props.value])
+    }, [props.value])
 
     function handleChange(text) {
         setValue(text)
-        if(props.onChange) props.onChange(text)
+        if (props.onChange) props.onChange(text)
     }
 
     return <>
-        <Input name={props.name} value={value} hidden invalid={props.invalid}/>
+
 
         <div className="row">
             <div className="col-md-6">
-                <Editor
+                <Input name={props.name} defaultValue={value} invalid={props.invalid} type="textarea" style={{height: '100%'}}/>
+                {/*<Editor
                     value={props.value}
                     locale='ru'
                     onChange={handleChange}
-                />
+                />*/}
             </div>
             <div className="col-md-6"><MarkDown source={value}/></div>
         </div>
