5dfb61b3081aec3bdbba5a4cf7eedad0b0acf482
abrikos <abrikoz@gmail.com>
2020-04-16 17:20:51 +0900

    virtual link

--- server/db/models/Model-Meeting.js
+++ server/db/models/Model-Meeting.js
@@ -1,13 +1,14 @@
 //import moment from "moment";
 
+import transliterate from "transliterate";
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const modelSchema = new Schema({
-        name: String,
-        path: String,
-        persons: [{type: mongoose.Schema.Types.ObjectId, ref: 'Person'}],
-        description: String,
+        name: {type:String, label:'Название'},
+        persons: [{type: mongoose.Schema.Types.ObjectId, ref: 'Person', label: 'Персоны'}],
+        description:  {type:String, label:'Описание'},
     },
     {
         //timestamps: {createdAt: 'createdAt'},
@@ -18,7 +19,11 @@ const modelSchema = new Schema({
     });
 modelSchema.statics.population = [{path: 'persons', populate: 'image'}];
 modelSchema.listFields = ['name'];
-modelSchema.formFields = ['name','path','description'];
+
+modelSchema.virtual('link')
+    .get(function () {
+        return `/council/` + this.id + '/' + (this.name ? transliterate(this.name).replace(/[^a-zA-Z0-9]/g, '-') : '')
+    });
 
 
 export default mongoose.model("Meeting", modelSchema)
