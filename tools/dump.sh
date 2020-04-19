ec3da936c0445dd634e402ef7b34eefe9064c5b0
abrikos <abrikoz@gmail.com>
2020-04-19 20:19:40 +0900

    dump dir

--- tools/dump.sh
+++ tools/dump.sh
@@ -2,5 +2,6 @@
 mongodump
 zip academy dump/academy/*
 #mv academy.zip build/.
-mv academy.zip /var/www/devportal.yakutia.science/web/
 unzip -l build/academy.zip
+mv academy.zip /var/www/devportal.yakutia.science/web/
+
