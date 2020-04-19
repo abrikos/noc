b2a974084eabcb8578d81f621d06babe1ef59a82
abrikos <abrikoz@gmail.com>
2020-04-19 15:17:17 +0900

    disable telegram

--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -23,7 +23,7 @@ export default function Login(props) {
 
                     {/*<Button onClick={() => props.logIn('test')}>Test</Button>*/}
 
-                    <TelegramLogin {...props}/>
+                    {/*<TelegramLogin {...props}/>*/}
 
                     <Button onClick={() => responseVk('vk')} color="light"><img src={vk} alt="В контакте" style={{width: 50}}/> </Button>
                     {/*<Button onClick={()=>responseVk('mailru')}>Mailru</Button>*/}
