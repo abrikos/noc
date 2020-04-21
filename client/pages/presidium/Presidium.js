ada60b53294d61303a842c8d4be635b843271626
abrikos <abrikoz@gmail.com>
2020-04-21 16:25:36 +0900

    fix presidium

--- client/pages/presidium/Presidium.js
+++ client/pages/presidium/Presidium.js
@@ -13,27 +13,30 @@ export default function Presidium(props) {
                     .then(res => setModel(res.list[0]))
                 break;
             default:
-                const ids={
-                    'president':[1,2],
-                    'vice':[3],
-                    'scsecretary':[4],
-                    'advisors':[4],
+                const types = {
+                    'president': {
+                        ids: [1, 2
+                        ], limit: 1
+                    },
+                    'vice': {ids: [3]},
+                    'scsecretary': {ids: [4], limit: 1},
+                    'advisors': {ids: [4]},
                 }
-                props.api('/person/list',{where:{presidiumStatusId:{$in:ids[props.type]}}, sort:{presidiumStatusId:1}, limit:1})
-                    .then(res=>setPersons(res.list))
+                props.api('/person/list', {where: {presidiumStatusId: {$in: types[props.type].ids}}, sort: {presidiumStatusId: 1}, limit: types[props.type].limt})
+                    .then(res => setPersons(res.list))
                 break;
         }
     }, [props.type])
 
-    const labels =['','Президент АН РС(Я)','И.о. Президента АН РС(Я)','Вице-президенты АН РС(Я)','Главный ученый секретарь АН РС(Я)','Советники АН РС(Я)']
-    if(persons) return <div key={props.type}>
+    const labels = ['', 'Президент АН РС(Я)', 'И.о. Президента АН РС(Я)', 'Вице-президенты АН РС(Я)', 'Главный ученый секретарь АН РС(Я)', 'Советники АН РС(Я)']
+    if (persons) return <div key={props.type}>
         <h1 className="text-center">{labels[persons[0].presidiumStatusId]}</h1>
-        <div  className="d-flex justify-content-center">
-        {persons.map(p=><PersonLarge person={p} {...props}/>)}
+        <div className="d-flex justify-content-center">
+            {persons.map(p => <PersonLarge person={p} {...props}/>)}
         </div>
     </div>
 
-    if(model) return <div>
+    if (model) return <div>
         <h1>Состав ученого совета Президиума Академии наук РС(Я)</h1>
         <div className="d-flex flex-wrap">
             {model.chief && <PersonSmall presidium={true} status={'Председатель'} person={model.chief} {...props}/>}
