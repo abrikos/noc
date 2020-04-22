4e6d06e15f3429d743145545f372de1d43370810
abrikos <abrikoz@gmail.com>
2020-04-22 16:29:07 +0900

    fix contacts

--- client/pages/home/BottomInfo.js
+++ client/pages/home/BottomInfo.js
@@ -23,6 +23,7 @@ export default function BottomInfo(props) {
             <h3>КОНТАКТЫ</h3>
             <Phone phone={'+7(4122)33-57-11'}/>
             <Email email={'secretary@yakutia.science'}/>
+            <Email email={'anrsya@mail.ru'}/>
             <Address link="https://2gis.ru/yakutsk/search/%D0%BF%D1%80-%D1%82%20%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0%2C%2033/firm/7037402698754322?queryState=center%2F129.724604%2C62.024556%2Fzoom%2F18" label="677007, г. Якутск, пр-т Ленина, 33"/>
         </div>
     </div>
