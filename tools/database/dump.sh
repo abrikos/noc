70d2287a5a765c7d6536d739db1a7b521d7c669a
abrikos <abrikoz@gmail.com>
2020-04-29 18:19:36 +0900

    fix tools

--- tools/database/dump.sh
+++ tools/database/dump.sh
@@ -3,5 +3,5 @@ mongodump
 zip academy dump/academy/*
 cp academy.zip build/.
 unzip -l build/academy.zip
-mv academy.zip /var/www/devportal.yakutia.science/web/
+#mv academy.zip /var/www/devportal.yakutia.science/web/
 
--- tools/database/restore.sh
+++ tools/database/restore.sh
@@ -9,7 +9,9 @@ mongorestore --drop
 
 ## fetch static files
 wget -O upload.idx https://academy.abrikos.pro/uploads/
-for file in `cat upload.idx | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d'`
+for file in `cat upload.idx | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d'`;
 do
-wget -nc -O "uploads/${file}" "https://academy.abrikos.pro/uploads/${file}"
+if [ ! -f "uploads/${file}" ]; then
+    wget -nc -O "uploads/${file}" "https://academy.abrikos.pro/uploads/${file}"
+fi
 done
