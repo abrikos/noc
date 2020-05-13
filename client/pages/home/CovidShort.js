ae89a6961321853435392da41be6567c9d827b14
abrikos <abrikoz@gmail.com>
2020-05-13 21:06:16 +0900

    fix covid date

--- client/pages/home/CovidShort.js
+++ client/pages/home/CovidShort.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import "./covid.sass"
 import {A} from "hookrouter"
+import moment from "moment"
 
 export default function (props) {
     const [dataSakha, setDataSakha] = useState()
@@ -9,6 +10,7 @@ export default function (props) {
         props.api('/covid', {where: {isRussia: false}})
             .then(res => {
                 setDataSakha(res)
+                console.log(res)
             })
     }, [])
 
@@ -16,7 +18,7 @@ export default function (props) {
     if(!dataSakha) return <div></div>
     return <div className="covid-short text-center mb-3">
         <A href="/covid19">
-        <strong>Covid-19</strong>. Оперативные&nbsp;данные&nbsp;по&nbsp;Якутии&nbsp;на&nbsp;<span>{dataSakha[dataSakha.length - 1].date}</span>.
+        <strong>Covid-19</strong>. Оперативные&nbsp;данные&nbsp;по&nbsp;Якутии&nbsp;на&nbsp;<span>{moment(dataSakha[dataSakha.length - 1].id).format('DD.MM.YY')}</span>.
         Случаев&nbsp;заболевания:&nbsp;<span className="text-danger" style={{fontSize:'2em'}}>{dataSakha[dataSakha.length - 1].new}</span>. {' '}
         </A>
     </div>
