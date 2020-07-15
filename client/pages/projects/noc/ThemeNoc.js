50a42774b8aa38f4e3879fd57d55bd48df979235
abrikos <abrikoz@gmail.com>
2020-07-15 12:20:29 +0900

    noc top

--- client/pages/projects/noc/ThemeNoc.js
+++ client/pages/projects/noc/ThemeNoc.js
@@ -4,16 +4,18 @@ import "client/components/themes/top-menu/theme-horizontal.sass"
 import logo from "./noc.svg"
 import logoAcademy from "client/images/logo-text.svg"
 import "./noc.sass"
+import top from "./top.png"
 import {A} from "hookrouter"
 
 export default function ThemeNoc(props) {
     return <div className="theme-noc container">
-        <div className=" header">
+        <img src={top} className="img-fluid"/>
+        {/*<div className=" header">
             <div className="row">
                 <div className="col-2 text-center"><img src={logo} alt="НОЦ лого" className="img-fluid m-2"/></div>
                 <div className="col-10 d-flex align-items-center"><h1>Научно-образовательный центр «Север: территория устойчивого развития»</h1></div>
             </div>
-        </div>
+        </div>*/}
         {props.errorPage || props.routeResult}
         <footer className="d-flex flex-wrap justify-content-around">
 
